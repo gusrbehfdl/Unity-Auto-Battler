bdcf5067667429ef84f35d52aa656aa3ddcd4aed
gusrbehfdl <16564906+gusrbehfdl@users.noreply.github.com>
2025-06-19 12:15:34 +0900

    assertion added to better detect error in mainmenu

--- Auto Battler/Assets/Auto-Battle Framework/Scripts/Skills/BasicAttackEffects/ArrowEffect.cs
+++ Auto Battler/Assets/Auto-Battle Framework/Scripts/Skills/BasicAttackEffects/ArrowEffect.cs
@@ -1,18 +1,14 @@
 using AutoBattleFramework.BattleBehaviour.GameActors;
 using AutoBattleFramework.Formulas;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Assertions;
 
-namespace AutoBattleFramework.Skills
-{
+namespace AutoBattleFramework.Skills {
     /// <summary>
     /// Simple projectile attack. The projectile moves in straight line until it reaches the target.
     /// </summary>
     [CreateAssetMenu(fileName = "ArrowEffect", menuName = "Auto-Battle Framework/Effects/RangedEffect/ArrowEffect", order = 4)]
-    public class ArrowEffect : RangedEffect
-    {
+    public class ArrowEffect : RangedEffect {
         /// <summary>
         /// The transform from which the projectile will be launched.
         /// </summary>
@@ -23,8 +19,7 @@ namespace AutoBattleFramework.Skills
         /// </summary>
         /// <param name="ai">Attacking GameCharacter</param>
         /// <param name="shootingPoint">The transform from which the projectile will be launched.</param>
-        public override void Attack(GameCharacter ai, Transform shootingPoint)
-        {
+        public override void Attack(GameCharacter ai, Transform shootingPoint) {
             this.ai = ai;
             this.shootingPoint = shootingPoint;
             SpawnProjectile();
@@ -36,8 +31,7 @@ namespace AutoBattleFramework.Skills
         /// Calls <see cref="Formulas.BattleFormulas.BasicAttackDamage"/>  when the projectile hits the target.
         /// </summary>
         /// /// <param name="target">The target of the projectile.</param>
-        public override void OnHit(GameCharacter target)
-        {
+        public override void OnHit(GameCharacter target) {
             BattleFormulas.BasicAttackDamage(DamageType, ai.TargetEnemy, ai);
         }
 
@@ -45,13 +39,15 @@ namespace AutoBattleFramework.Skills
         /// Instantiate the Projectile and sets its properties.
         /// </summary>
         /// <returns>The Instantiated projectile</returns>
-        protected override Projectile SpawnProjectile()
-        {
+        protected override Projectile SpawnProjectile() {
+            Assert.IsNotNull(ai);
+            Assert.IsNotNull(ai.TargetEnemy);
+            Assert.IsNotNull(ai.TargetEnemy.EnemyShootingPoint);
             Projectile proj = Instantiate(projectile) as Projectile;
             proj.transform.position = shootingPoint.transform.position;
             proj.transform.LookAt(ai.TargetEnemy.EnemyShootingPoint);
             proj.SetTarget(ai, ai.TargetEnemy, speed, this);
-            
+
             return proj;
         }
     }
